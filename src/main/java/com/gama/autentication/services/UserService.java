757615709d83f0ac65ba41108526d09daaee6ad9
Adelmo Gama <109219336+adelmoGama@users.noreply.github.com>
2023-03-21 12:26:12 -0300

    Imagens

--- src/main/java/com/gama/autentication/services/UserService.java
+++ src/main/java/com/gama/autentication/services/UserService.java
@@ -38,10 +38,16 @@ public class UserService {
 	}
 	
 	public ResponseEntity<Boolean> autentication (@RequestParam String email, @RequestParam String tryPassword) {
+		// CONSULTANDO O USUÁRIO E VERIFICANDO SE O MESMO FOI ENCONTRADO PELO EMAIL. CASO NÃO TENHA SIDO JÁ RETORNO O NÃO AUTORIZADO COM O VALOR FALSO.
 		Optional<User> user = findByEmail(email);
 		if(user.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
 		}
+		/*
+		 * SE O USUÁRIO FOR ENCONTRADO ELE VAI SER BUSCADO PARA A VERIFICAÇÃO DA SENHA CRIPTOGRAFADA. PARA ISSO A SENHA PASSADA SERÁ COMPARADA 
+		 * COM A DO USUÁRIO ENCONTRADO PELO MÉTODO 'matches' do encoder.
+		 * CASO AS SENHAS SEJAM IGUAIS O STATUS RETORNADO SERÁ O 'OK' E CASO NÃO SERÁ RETORNADO O 'UNAUTHORIZES'.
+		 */
 		User userAutorized = user.get();
 		boolean validation = encoder.matches(tryPassword, userAutorized.getPassword());
 		HttpStatus status = (validation) ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
